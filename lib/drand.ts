18670a9ff9acaee23bb85a810b3a57ad64223dbd
Kevin Charm <kevin@kevincharm.com>
2024-02-11 14:28:01 +0100

    update drand devnet info

--- lib/drand.ts
+++ lib/drand.ts
@@ -2,15 +2,13 @@ import { getBytes, hexlify } from 'ethers'
 
 export const DRAND_BN254_INFO = {
     public_key:
-        '1731c37e5e9d24c748d465dfacdfd0c8ad71150677d0086eba61d96d452a034912a4f6666e59df44dc6a0b2cdcae89137c1a594bdb25be7a2c81f4bf7634763121ebf198e661a8da76492bf629fd63fc8b3e71b9f5a11653e009e84f58bb2fbe2c50f58ab080f8e54f8cc5bc1f591a737f2c611a35cff799df69f38f2d886745',
-    period: 1,
-    genesis_time: 1702432747,
-    hash: 'e566fb7b16fa767962b00ae623ce9fd610b8b2df48ea573c0258f7a5fbf36afd',
-    groupHash: 'f5d54bfd6fd0ab42d881c3536ef7a7e0dcd443c51b65c8a67a2f8ba77c8126a8',
+        '132129f8c30a43d4c00d2d6b1f6cabd3184be62f829846301c805ba8eb8570d81c80c7ab94dbf2fb22ebee71ab2b6f79d27d445d3d6ddc2ef1d28df86955c0eb1aad8f4a986b9a4157543ef2145bbf773863dc5acbfec48a5ff8078d6efecf9e00c74538d9f890a2c177633c7f88a4cb27dc4407decc55a770b5ecbcda759010',
+    period: 3,
+    genesis_time: 1707657321,
+    hash: 'ee170bf15d789de7c3264dcea5b214e2745f20833a0b276bdc8db73522342e8e',
+    groupHash: '016c244897664763288dc1afde9d9bef453c99026fa6314110c12e1452e96002',
     schemeID: 'bls-bn254-unchained-on-g1',
-    metadata: {
-        beaconID: 'fairy-drand-bn254-dev',
-    },
+    metadata: { beaconID: 'fairy-drand-bn254-dev' },
 }
 
 export function decodeG2(point: string) {
