d595221ef64e9a0d4899b149ec815b1410d3104e
Kevin Charm <kevin@kevincharm.com>
2024-10-10 23:08:22 +0200

    separate out network configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -30,78 +30,11 @@ const config: HardhatUserConfig = {
             },
             initialBaseFeePerGas: 1,
         },
-        scroll: {
-            chainId: 534352,
-            url: process.env.SCROLL_URL as string,
-            accounts: [process.env.MAINNET_PK as string],
-        },
-        scrollSepolia: {
-            chainId: 534351,
-            url: process.env.SCROLL_SEPOLIA_URL as string,
-            accounts: [process.env.MAINNET_PK as string],
-        },
-        sepolia: {
-            chainId: 11155111,
-            url: process.env.SEPOLIA_URL as string,
-            accounts: [process.env.MAINNET_PK as string],
-        },
-        base: {
-            chainId: 8453,
-            url: process.env.BASE_URL as string,
-            accounts: [process.env.MAINNET_PK as string],
-        },
-        degen: {
-            chainId: 666666666,
-            url: process.env.DEGEN_URL as string,
-            accounts: [process.env.MAINNET_PK as string],
-        },
-        gnosis: {
-            chainId: 100,
-            url: process.env.XDAI_URL as string,
-            accounts: [process.env.MAINNET_PK as string],
-        },
     },
     gasReporter: {
         enabled: true,
         currency: 'USD',
         gasPrice: 60,
-    },
-    etherscan: {
-        apiKey: {
-            mainnet: process.env.ETHERSCAN_API_KEY as string,
-            scroll: process.env.SCROLLSCAN_API_KEY as string,
-            scrollSepolia: process.env.SCROLLSCAN_API_KEY as string,
-            sepolia: process.env.ETHERSCAN_API_KEY as string,
-            base: process.env.BASESCAN_API_KEY as string,
-            xdai: process.env.GNOSISSCAN_API_KEY as string,
-            degen: 'abc', // blockscout
-        },
-        customChains: [
-            {
-                network: 'scroll',
-                chainId: 534352,
-                urls: {
-                    apiURL: 'https://api.scrollscan.com/api',
-                    browserURL: 'https://scrollscan.com',
-                },
-            },
-            {
-                network: 'scrollSepolia',
-                chainId: 534351,
-                urls: {
-                    apiURL: 'https://api-sepolia.scrollscan.com/api',
-                    browserURL: 'https://sepolia.scrollscan.dev',
-                },
-            },
-            {
-                network: 'degen',
-                chainId: 666666666,
-                urls: {
-                    apiURL: 'https://explorer.degen.tips/api',
-                    browserURL: 'https://explorer.degen.tips',
-                },
-            },
-        ],
     },
     contractSizer: {
         alphaSort: true,
